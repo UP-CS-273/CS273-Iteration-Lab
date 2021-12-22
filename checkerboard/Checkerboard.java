c17860831727081c9c47a125b9ebb4bee35ffb23
upcsfaculty <91204735+upcsfaculty@users.noreply.github.com>
2021-12-22 09:34:00 -0800

    Initial Commit

---
+++ checkerboard/Checkerboard.java
@@ -0,0 +1,31 @@
+/*****************************************************************
+ * Checkerboard.java - Defines a Java app that draws some boxes
+ *
+ * Name: ****PUT YOUR NAME HERE****
+ * Date: ****PUT THE DATE OF COMPLETTION HERE****
+ * Status: incomplete
+ *****************************************************************/
+
+import java.awt.*;
+
+/*****************************************************************
+ * The class Checkerboard displays an NxN checkerboard on the screen. It extends
+ * the CheckerboardMain class which handles all the mouse interactions.
+ *
+ * This class inherits the numSquares variable from CheckerboardMain. That
+ * variable specifies the number of squares along one side of the checkerboard.
+ * So, for an NxN checkerboard, numSquares = N. The CheckerboardMain class will
+ * modify this variable in response to mouse-clicks.
+ *****************************************************************/
+public class Checkerboard extends CheckerboardMain {
+
+    /**
+     * Paints the app's image on the graphics window.
+     *
+     * @param g - The app's Graphics object
+     */
+    public void drawCheckerboard(Graphics g) {
+        
+        //Do nothing for now
+    }//drawCheckerboard
+}//class Checkerboard
